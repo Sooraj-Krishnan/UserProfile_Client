ec055853da3d0bcc70c658cab0da9f009e94dd73
Sooraj-Metasoft <sooraj@metasoftit.com>
2024-01-16 18:25:08 +0530

    create menu function

---
+++ src/components/menuView/MenuFunction.jsx
@@ -0,0 +1,67 @@
+// menuFunctions.js
+
+export const incrementQuantity = (
+  itemId,
+  queryClient,
+  id,
+  setLocalMenuData
+) => {
+  queryClient.setQueryData(["menuView", id], (oldData) => {
+    const updatedMenuData = { ...oldData };
+    for (
+      let menuItemIndex = 0;
+      menuItemIndex < updatedMenuData.menuItems.length;
+      menuItemIndex++
+    ) {
+      const itemIndex = updatedMenuData.menuItems[
+        menuItemIndex
+      ].items.findIndex((item) => item._id === itemId);
+      if (itemIndex !== -1) {
+        updatedMenuData.menuItems[menuItemIndex].items[itemIndex].quantity++;
+        break;
+      }
+    }
+    return updatedMenuData;
+  });
+
+  setLocalMenuData(queryClient.getQueryData(["menuView", id]));
+};
+
+export const decrementQuantity = (
+  itemId,
+  queryClient,
+  id,
+  setLocalMenuData
+) => {
+  queryClient.setQueryData(["menuView", id], (oldData) => {
+    const updatedMenuData = { ...oldData };
+    for (
+      let menuItemIndex = 0;
+      menuItemIndex < updatedMenuData.menuItems.length;
+      menuItemIndex++
+    ) {
+      const itemIndex = updatedMenuData.menuItems[
+        menuItemIndex
+      ].items.findIndex((item) => item._id === itemId);
+      if (
+        itemIndex !== -1 &&
+        updatedMenuData.menuItems[menuItemIndex].items[itemIndex].quantity > 1
+      ) {
+        updatedMenuData.menuItems[menuItemIndex].items[itemIndex].quantity--;
+        break;
+      }
+    }
+    return updatedMenuData;
+  });
+
+  setLocalMenuData(queryClient.getQueryData(["menuView", id]));
+};
+
+export const addToCart = (itemId, menuData, cartItems, setCartItems) => {
+  const selectedItem = menuData.menuItems
+    .flatMap((menuItem) => menuItem.items)
+    .find((item) => item._id === itemId);
+  const newCartItems = [...cartItems, selectedItem];
+  setCartItems(newCartItems);
+  localStorage.setItem("cartItems", JSON.stringify(newCartItems));
+};
--- src/components/menuView/MenuView.jsx
+++ src/components/menuView/MenuView.jsx
@@ -2,15 +2,23 @@ import { useState, useEffect } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useParams, useNavigate } from "react-router-dom";
 import { menuView } from "../../api/PublicRequest";
-import { Typography, Row, Col } from "antd";
+import { Typography, Row, Col, Input, Space } from "antd";
 import { FaShoppingCart } from "react-icons/fa";
 import "./MenuView.css";
+import {
+  incrementQuantity,
+  decrementQuantity,
+  addToCart,
+} from "./MenuFunction";
+
 const { Title } = Typography;
+const { Search } = Input;
 
 const MenuView = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [selectedLabel, setSelectedLabel] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
   const [cartItems, setCartItems] = useState([]);
   const [, setLocalMenuData] = useState(null);
   const queryClient = useQueryClient();
@@ -47,79 +55,47 @@ const MenuView = () => {
   if (isError) {
     return <div>Error occurred while fetching data</div>;
   }
-
-  const incrementQuantity = (itemId) => {
-    queryClient.setQueryData(["menuView", id], (oldData) => {
-      const updatedMenuData = { ...oldData };
-      for (
-        let menuItemIndex = 0;
-        menuItemIndex < updatedMenuData.menuItems.length;
-        menuItemIndex++
-      ) {
-        const itemIndex = updatedMenuData.menuItems[
-          menuItemIndex
-        ].items.findIndex((item) => item._id === itemId);
-        if (itemIndex !== -1) {
-          updatedMenuData.menuItems[menuItemIndex].items[itemIndex].quantity++;
-          break;
-        }
-      }
-      return updatedMenuData;
-    });
-
-    setLocalMenuData(queryClient.getQueryData(["menuView", id]));
-  };
-
-  const decrementQuantity = (itemId) => {
-    setLocalMenuData((prevData) => {
-      const updatedMenuData = { ...prevData };
-      for (
-        let menuItemIndex = 0;
-        menuItemIndex < updatedMenuData.menuItems.length;
-        menuItemIndex++
-      ) {
-        const itemIndex = updatedMenuData.menuItems[
-          menuItemIndex
-        ].items.findIndex((item) => item._id === itemId);
-        if (
-          itemIndex !== -1 &&
-          updatedMenuData.menuItems[menuItemIndex].items[itemIndex].quantity > 1
-        ) {
-          updatedMenuData.menuItems[menuItemIndex].items[itemIndex].quantity--;
-          break;
-        }
-      }
-      queryClient.setQueryData(["menuView", id], updatedMenuData);
-      return updatedMenuData;
-    });
-  };
-
-  const addToCart = (itemId) => {
-    const selectedItem = menuData.menuItems
-      .flatMap((menuItem) => menuItem.items)
-      .find((item) => item._id === itemId);
-    const newCartItems = [...cartItems, selectedItem];
-    setCartItems(newCartItems);
-    localStorage.setItem("cartItems", JSON.stringify(newCartItems));
-  };
+  const onSearch = (value) => setSearchTerm(value);
 
   return (
     <div
-      className="bg-gray-200"
+      className="bg-gray-100"
       style={{
         width: "390px",
         margin: "0 auto",
       }}
     >
+      <Space direction="vertical">
+        <Search
+          placeholder="search for dishes"
+          onSearch={onSearch}
+          style={{
+            width: 300,
+          }}
+        />
+      </Space>
+
       <div className="flex space-x-4 justify-center">
         <div className="rectangle-button">
-          <button onClick={() => setSelectedLabel(null)}>All</button>
+          <button
+            onClick={() => {
+              setSelectedLabel(null);
+              setSearchTerm("");
+            }}
+          >
+            All
+          </button>
         </div>
         {menuData &&
           menuData.menuItems &&
           menuData.menuItems.map((menuItem, index) => (
             <div className="rectangle-button" key={index}>
-              <button onClick={() => setSelectedLabel(menuItem.label)}>
+              <button
+                onClick={() => {
+                  setSelectedLabel(menuItem.label);
+                  setSearchTerm("");
+                }}
+              >
                 {menuItem.label}
               </button>
             </div>
@@ -133,55 +109,99 @@ const MenuView = () => {
         {cartItems.length}
       </div>
 
-      {menuData &&
-        menuData.menuItems &&
-        (selectedLabel
-          ? menuData.menuItems.filter(
-              (menuItem) => menuItem.label === selectedLabel
-            )
-          : menuData.menuItems
-        ).map((menuItem, menuItemIndex) => (
-          <div key={menuItemIndex} className="m-8">
-            <Title level={1}>{menuItem.label}</Title>
-            {menuItem.items.map((item, itemIndex) => {
-              const [price, unit] = item.price.split(" ");
+      {(() => {
+        let itemsFound = false;
+        const menuItems =
+          menuData &&
+          menuData.menuItems &&
+          (selectedLabel
+            ? menuData.menuItems.filter(
+                (menuItem) => menuItem.label === selectedLabel
+              )
+            : menuData.menuItems
+          ).map((menuItem, menuItemIndex) => {
+            const filteredItems = menuItem.items.filter((item) =>
+              item.itemName.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+
+            if (filteredItems.length > 0) {
+              itemsFound = true;
               return (
-                <Row key={itemIndex}>
-                  <Col span={12}>
-                    <Title level={3}>{item.itemName}</Title>
-                    <p>{item.description}</p>
-                    <p>
-                      Price: {parseInt(price) * item.quantity} {unit}
-                    </p>
-                    <div className="quantity-button">
-                      {/* Quantity Adjustment Buttons */}
-                      <button onClick={() => decrementQuantity(item._id)}>
-                        -
-                      </button>
-                      <span>{item.quantity}</span>
-                      <button onClick={() => incrementQuantity(item._id)}>
-                        +
-                      </button>
-                    </div>
-                    <div className="quantity-button mt-2 justify-center">
-                      {/* <button onClick={() => addToCart(item)}>Add</button> */}
-                      <button onClick={() => addToCart(item._id)}>Add</button>
-                    </div>
-                  </Col>
-                  <Col span={12}>
-                    <img
-                      className="m-2"
-                      src={item.itemImage}
-                      alt={item.itemName}
-                    />
-                  </Col>
-                </Row>
+                <div key={menuItemIndex} className="m-8">
+                  <Title level={1}>{menuItem.label}</Title>
+                  {filteredItems.map((item, itemIndex) => {
+                    const [price, unit] = item.price.split(" ");
+                    return (
+                      <Row key={itemIndex}>
+                        <Col span={12}>
+                          <Title level={3}>{item.itemName}</Title>
+                          <p>{item.description}</p>
+                          <p>
+                            Price: {parseInt(price) * item.quantity} {unit}
+                          </p>
+                          <div className="quantity-button">
+                            {/* Quantity Adjustment Buttons */}
+                            <button
+                              onClick={() =>
+                                decrementQuantity(
+                                  item._id,
+                                  queryClient,
+                                  id,
+                                  setLocalMenuData
+                                )
+                              }
+                            >
+                              -
+                            </button>
+                            <span>{item.quantity}</span>
+                            <button
+                              onClick={() =>
+                                incrementQuantity(
+                                  item._id,
+                                  queryClient,
+                                  id,
+                                  setLocalMenuData
+                                )
+                              }
+                            >
+                              +
+                            </button>
+                          </div>
+                          <div className="quantity-button mt-2 justify-center">
+                            {/* <button onClick={() => addToCart(item)}>Add</button> */}
+                            <button
+                              onClick={() =>
+                                addToCart(
+                                  item._id,
+                                  menuData,
+                                  cartItems,
+                                  setCartItems
+                                )
+                              }
+                            >
+                              Add
+                            </button>
+                          </div>
+                        </Col>
+                        <Col span={12}>
+                          <img
+                            className="m-2"
+                            src={item.itemImage}
+                            alt={item.itemName}
+                          />
+                        </Col>
+                      </Row>
+                    );
+                  })}
+                </div>
               );
-            })}
-          </div>
-        ))}
+            }
+            return null;
+          });
+
+        return itemsFound ? menuItems : <div>No items found...</div>;
+      })()}
     </div>
   );
 };
-
 export default MenuView;
